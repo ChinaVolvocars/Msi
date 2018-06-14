79f69baf9a3a9f12a56a39502aec6cf965d775e1
Tiger <atlantisspeed@gmail.com>
2018-06-15 00:39:07 +0800

    Toast

--- common/src/main/java/com/example/common/Utils.java
+++ common/src/main/java/com/example/common/Utils.java
@@ -1,10 +1,17 @@
 package com.example.common;
 
-import java.sql.SQLOutput;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.widget.Toast;
 
 public class Utils {
 
-  public static void print() {
-    System.out.println("我是库里的！！！");
+  public static void toast(Context context, @StringRes int msg) {
+    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+  }
+
+  public static void toast(Context context, @Nullable String msg) {
+    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
   }
 }
